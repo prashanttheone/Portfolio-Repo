860af7cf965d199a7fe3632865ad545ca0ded132
PRASHANT KUMAR <126236297+prashanttheone@users.noreply.github.com>
2024-01-21 02:08:53 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const dbConnect = () => {
-  mongoose.connect(process.env.DATABASE_URL, {
+   await mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
