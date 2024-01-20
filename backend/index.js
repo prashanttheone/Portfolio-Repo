26ba66ce5d860106e7f9e60c9d156664d64b2fe4
PRASHANT KUMAR <126236297+prashanttheone@users.noreply.github.com>
2024-01-21 02:27:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use('/contact', contactRoutes);
 
 // Generic error handler middleware
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
