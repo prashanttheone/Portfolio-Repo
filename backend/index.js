f728029e16cf4d91cca2f55ffa93b93eb9411e15
PRASHANT KUMAR <126236297+prashanttheone@users.noreply.github.com>
2024-01-21 02:21:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 3000 ;
 dbConnect(); // Connect to the database
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
