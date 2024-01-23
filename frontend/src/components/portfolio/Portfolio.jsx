848e1732fa9d7d7040dda0553be3ab21e9d6aedb
PRASHANT KUMAR <126236297+prashanttheone@users.noreply.github.com>
2024-01-23 12:16:52 +0530

    Update Portfolio.jsx

--- frontend/src/components/portfolio/Portfolio.jsx
+++ frontend/src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import IMG1 from '../../assets/alc.png';
-import IMG2 from '../../assets/todo.png';
-import IMG3 from '../../assets/shelter.png';
-import IMG4 from '../../assets/fake.png';
+import IMG1 from '../../assets/movix.jpg';
+import IMG4 from '../../assets/cal.jpg';
 import IMG5 from '../../assets/news.png';
-import IMG6 from '../../assets/math.png';
 
 import './portfolio.css';
 
@@ -12,36 +9,33 @@ const Portfolio = () => {
   const soloProjects = [
     {
       id: 1,
-      title: 'Adventist Learning Center',
+      title: 'Movix',
       img: IMG1,
       description:
-        'Adventist Learning Center provides quality education to Syrian and Kurdish Refugees living in Beirut, Lebanon. This simple website was built to reach a bigger audience and ease the process of gathering donations for the ALC students.',
-      technologies: 'Ruby on Rails | SCSS',
-      link: 'https://adventistlearningcenter-257375ab1970.herokuapp.com/',
-      github: 'https://github.com/Meri-MG/school',
+        'Frontend Development: Led the development of the frontend using React and Redux, ensuring a responsive and user-friendly interface for seamless navigation.\n\nAPI Integration: Successfully integrated the TMBD API, allowing Movix to dynamically fetch and display up-to-date movie information, including details and trailers.',
+      technologies: 'React, API | SCSS',
+      link: 'https://movix-chi-three.vercel.app/',
+      github: 'https://github.com/prashanttheone/MOVIX',
     },
     {
       id: 2,
-      title: 'FakeStore',
+      title: 'Calculator',
       img: IMG4,
       description:
-        'FakeStore app that provides information about available products and their details',
-      technologies: 'React | Redux',
-      link: 'https://fakestore-metrics.netlify.app/',
-      github: 'https://github.com/Meri-MG/Fakestore---react',
+        'Developed a dynamic calculator using JavaScript, providing users with a seamless and efficient tool for basic arithmetic operations. The calculator features a clean and intuitive user interface, allowing for easy input and displaying accurate results in real-time. This project showcases my proficiency in JavaScript for creating interactive and functional web applications.',
+      technologies: 'JavaScript | HTML CSS',
+      link: 'https://a75fddf0-787e-4675-8d9b-efbdb192d51a-00-3avh3tg442zkl.kirk.replit.dev/',
+      github: '#',
     },
     {
       id: 3,
       title: 'To-Do App',
-      img: IMG1,
-      description: 'User friendly app to plan your day and manage tasks',
+      img: IMG5,
+      description: 'User-friendly app to plan your day and manage tasks.',
       technologies: 'JavaScript | Webpack',
-      link: 'https://meri-mg.github.io/To-Do-List/dist/',
-      github: 'https://github.com/Meri-MG/To-Do-List',
+      link: '#',
+      github: '#',
     },
-   
-   
-    
   ];
 
   return (
